4f75640064fea66dda9089647b7c8de669525054
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-03-02 00:20:26 +0700

    Add tests for CheckAccess middleware

--- lib/Plack/Middleware/PeriAHS/CheckAccess.pm
+++ lib/Plack/Middleware/PeriAHS/CheckAccess.pm
@@ -7,21 +7,69 @@ use warnings;
 use parent qw(Plack::Middleware);
 use Plack::Request;
 use Plack::Util::Accessor qw(
-                                allow_logs
-                                allowed_uri_schemes
-                                allowed_uris
-                                allowed_actions
+                                allow_log
+                                allow_uri_scheme
+                                deny_uri_scheme
+                                allow_uri
+                                deny_uri
+                                allow_action
+                                deny_action
                         );
 use Plack::Util::PeriAHS qw(errpage);
+use SHARYANTO::Array::Util qw(match_array_or_regex);
+use URI;
 
 # VERSION
 
 sub prepare_app {
     my $self = shift;
 
-    $self->{allow_logs}          //= 0;
-    $self->{allowed_uri_schemes} //= ['pm'];
-    #$self->{allowed_uris};
+    $self->{allow_log}         //= 0;
+    $self->{allow_uri_scheme}  //= ['pm'];
+}
+
+sub call {
+    my ($self, $env) = @_;
+
+    my $rr = $env->{"riap.request"};
+    my $uri = $rr->{uri};
+
+    if (!$self->{allow_log}) {
+        return errpage($env, [403, "Setting loglevel is forbidden"])
+            if $rr->{loglevel};
+    }
+
+    if ($self->{allow_uri_scheme}) {
+        my $sch = $uri->scheme;
+        return errpage($env, [403, "Riap URI scheme not allowed (not in list)"])
+            unless match_array_or_regex($sch, $self->{allow_uri_scheme});
+    }
+    if ($self->{deny_uri_scheme}) {
+        my $sch = $uri->scheme;
+        return errpage($env, [403, "Riap URI scheme not allowed (deny list)"])
+            if match_array_or_regex($sch, $self->{deny_uri_scheme});
+    }
+
+    if ($self->{allow_uri}) {
+        return errpage($env, [403, "Riap URI not allowed (not in list)"])
+            unless match_array_or_regex("$uri", $self->{allow_uri});
+    }
+    if ($self->{deny_uri}) {
+        return errpage($env, [403, "Riap URI not allowed (deny list)"])
+            if match_array_or_regex("$uri", $self->{deny_uri});
+    }
+
+    if ($self->{allow_action}) {
+        return errpage($env, [403, "Riap action not allowed (not in list)"])
+            unless match_array_or_regex($rr->{action}, $self->{allow_action});
+    }
+    if ($self->{deny_action}) {
+        return errpage($env, [403, "Riap action '$rr->{action}' not allowed (deny list)"])
+            if match_array_or_regex($rr->{action}, $self->{deny_action});
+    }
+
+    # continue to app
+    $self->app->($env);
 }
 
 1;
@@ -32,27 +80,43 @@ sub prepare_app {
 This middleware denies access according to some criterias in
 C<$env->{"riap.request"}>. It should be put after ParseRequest.
 
+For a more sophisticated access control, try the PeriAHS::ACL middleware.
+
 
 =head1 CONFIGURATIONS
 
 =over 4
 
-=item * allow_logs => BOOL (default 1)
+=item * allow_log => BOOL (default 1)
 
 Whether to allow request for returning log messages (request key C<loglevel>
 with values larger than 0). You might want to turn this off on production
 servers.
 
-=item * allowed_uri_schemes => ARRAY|REGEX (default ['pm'])
+=item * allow_uri_scheme => ARRAY|REGEX (default ['pm'])
 
 Which URI schemes are allowed. By default only local schemes are allowed. Add
 'http' or 'https' if you want proxying capability.
 
-=item * allowed_uris => ARRAY|REGEX (default ['pm'])
+=item * deny_uri_scheme => ARRAY|REGEX
 
-=item * allowed_actions => ARRAY|REGEX
+Which URI schemes are forbidden.
+
+=item * allow_uri => ARRAY|REGEX (default ['pm'])
+
+Allowed URIs. Note that URIs are normalized with scheme C<pm> if unschemed.
+Example:
+
+=item * deny_uri => ARRAY|REGEX (default ['pm'])
+
+Forbidden URIs.
+
+=item * allow_action => ARRAY|REGEX
 
 Which actions are allowed.
 
-=back
+=item * deny_action => ARRAY|REGEX
 
+Which actions are forbidden.
+
+=back
